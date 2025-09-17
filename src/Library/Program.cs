51b531089f17be0b852bfaa7dfa944fe31b23264
axelferreira100 <GUSTAVO.FERREIRA@correo.ucu.edu.uy>
2025-09-16 22:23:06 -0300

    Challenge completed, the bomb has been planted B)

--- src/Library/Program.cs
+++ src/Library/Program.cs
@@ -1,9 +1,34 @@
-﻿namespace Library
+﻿using System;
+using System.Collections.Generic;
+
+namespace Library
 {
     public class Program
     {
-        public static string Bomb(string txt)
+        public static string Bomb(string text)
         {
+            string[] listOfWordsInText = text.Split(" ");
+            List<string> listOfSymbols = new List<string> { ".", ",", "-", "?", "¿", "¡", "!", "_", ":", ";" };
+
+            foreach (string word in listOfWordsInText)
+            {
+                string fixedWord = "";
+
+                foreach (char character in word)
+                {
+                    if (listOfSymbols.IndexOf(character.ToString()) == -1)
+                    {
+                        fixedWord += character;
+                    }
+                }
+
+                if (fixedWord.ToLower() == "bomb")
+                {
+                    return "Duck!!!";
+                }
+            }
+
+            return "There is no bomb, relax.";
         }
     }
 }
